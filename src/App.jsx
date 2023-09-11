b0a833b3b2ec84aa120392b8d1528eeda77fc6eb
Sijan Malla <sijan.malla101@gmail.com>
2023-09-11 11:58:15 -0500

    Fetched Data
    Fetched the data using fetch() and promise. Setup the Loading Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react';
+import Loading from './Loading';
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [profiles, setProfiles] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProfiles(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  console.log(profiles);
   return <h2>Tabs Starter</h2>;
 };
 export default App;
